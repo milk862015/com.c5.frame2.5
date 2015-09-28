d7ecfc0f69cc72361b538fa9dfee4dd399850906
milk86 <milk86@yeah.net>
2015-09-28 15:37:32 +0800

    初始化项目

---
+++ bin-debug/Main.js
@@ -0,0 +1,33 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var __define = this.__define || function (o, p, g, s) {   Object.defineProperty(o, p, { configurable:true, enumerable:true, get:g,set:s }) };
+var Main = (function (_super) {
+    __extends(Main, _super);
+    function Main() {
+        _super.apply(this, arguments);
+    }
+    var d = __define,c=Main;p=c.prototype;
+    p.createChildren = function () {
+        _super.prototype.createChildren.call(this);
+        var theme = new eui.Theme("resource/default.thm.json", this.stage);
+        var button = new eui.Button();
+        button.label = "Click!";
+        button.horizontalCenter = 0;
+        button.verticalCenter = 0;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+    };
+    p.onButtonClick = function (e) {
+        var panel = new eui.Panel();
+        panel.title = "Title";
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        this.addChild(panel);
+    };
+    return Main;
+})(eui.UILayer);
+egret.registerClass(Main,"Main");
