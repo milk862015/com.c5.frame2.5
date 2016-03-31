f7b06afc3e492e6ead87349be6bd7cc5fe1b18dd
milk86 <milk86@yeah.net>
2016-03-31 13:45:25 +0800

    修改UIPanel

--- src/component/ui/UIPanel.ts
+++ src/component/ui/UIPanel.ts
@@ -3,26 +3,51 @@
  */
 class UIPanel extends SkinPanel{
     private uiLst:any;
+    protected cLst:any;
     constructor(){
         super();
     }
 
     protected partAdded(partName: string, instance: any): void{
         if( this.uiLst == void 0 ){ this.uiLst = {} }
+        if( this.cLst == void 0 ){this.cLst = {}}
         var ta:egret.DisplayObject = <egret.DisplayObject>instance;
         this.uiLst[partName] = ta;
     }
 
+    protected configHandler():void{
+
+    }
+
+    protected initData():void{
+
+    }
+
+    protected updateView():void{
+
+    }
+
     protected onAdd():void{
+        this.initData();
+        this.configHandler();
+        this.updateView();
         this.addHandler();
     }
 
     protected addHandler():void{
         for( var key in this.uiLst ){
-            var handler:any = this["on_" + key + "_handler"];
+            var handler:any = this.cLst[key];
             if( handler != void 0 ){
                 this.registerListener(this.uiLst[key],egret.TouchEvent.TOUCH_TAP,handler,this);
             }
         }
+    }
+
+    protected onCloseHandler( e:egret.TouchEvent ):void{
+        Core.PopUpLayer.RemovePopUp(this);
+    }
+
+    protected getUI(key:string):any{
+        return this.uiLst[key];
     }
 }
