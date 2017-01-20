26318128660b4a926f36f6e87c7a9b07f53a564c
milk86 <milk86@yeah.net>
2017-01-20 10:38:47 +0800

    优化音乐播放

--- src/layer/MusicLayer.ts
+++ src/layer/MusicLayer.ts
@@ -1,19 +1,17 @@
 /**
  * Created by Administrator on 2015/11/2.
  */
-class MusicLayer extends eui.Group{
-    static BG_WIDTH:number = 48;
-    static BG_HEIGHT:number = 48;
-    private btn:egret.Bitmap;
-    private _isPlay:boolean;
-    private _sound:egret.Sound;
-    private _chanel:egret.SoundChannel;
-    constructor(){
+class MusicLayer extends eui.Group {
+    static BG_WIDTH: number = 48;
+    static BG_HEIGHT: number = 48;
+    private btn: egret.Bitmap;
+    private _isPlay: boolean;
+    constructor() {
         super();
         this.initialize();
     }
 
-    private initialize():void{
+    private initialize(): void {
         this.btn = new egret.Bitmap();
         this.btn.anchorOffsetX = MusicLayer.BG_WIDTH * 0.5;
         this.btn.anchorOffsetY = MusicLayer.BG_HEIGHT * 0.5;
@@ -21,36 +19,49 @@ class MusicLayer extends eui.Group{
         this.btn.touchEnabled = true;
         this.addChild(this.btn);
         this.btn.x = 700;
-        this.btn.y = 50;
-        this.btn.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchHandler,this);
+        this.btn.y = 60;
+        this.btn.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onTouchHandler, this);
 
-        this._sound = RES.getRes("bg_mp3");
     }
 
-    private onTouchHandler( e:egret.TouchEvent ):void{
+    private onTouchHandler(e: egret.TouchEvent): void {
         this.IsPlay = !this.IsPlay;
     }
 
 
-    public set IsPlay(value:boolean){
-        if( this._isPlay != value ){
+    public set IsPlay(value: boolean) {
+        this.btn.x = Core.Stage.stageWidth * 0.5 + 325;
+        if (this._isPlay != value) {
             this._isPlay = value;
-            if( this._isPlay ){
-                this._chanel = this._sound.play();
+            if (this._isPlay) {
+                MusicManage.PlayMusic("bg_mp3", MusicManage.MusicSys.BACKGROUND);
                 this.btn.texture = RES.getRes("play_png");
-                egret.Tween.get(this.btn,{loop:true}).to({rotation:359},1000);
-            }else{
-                egret.Tween.removeTweens(this.btn);
+                this.run();
+            } else {
+                this.stop();
                 this.btn.rotation = 0;
                 this.btn.texture = RES.getRes("stop_png");
-                if( this._chanel ){
-                    this._chanel.stop();
-                }
+                MusicManage.StopMusic("bg_mp3");
             }
         }
     }
 
-    public get IsPlay():boolean{
+    private run(): void {
+        Core.Stage.addEventListener(egret.Event.ENTER_FRAME, this.runHandler, this);
+    }
+
+    private runHandler(e: egret.Event): void {
+        this.btn.rotation += 5;
+        if (this.btn.rotation >= 360) {
+            this.btn.rotation = this.btn.rotation - 360;
+        }
+    }
+
+    private stop(): void {
+        Core.Stage.removeEventListener(egret.Event.ENTER_FRAME, this.runHandler, this);
+    }
+
+    public get IsPlay(): boolean {
         return this._isPlay;
     }
 }
--- src/manage/MusicManager.ts
+++ src/manage/MusicManager.ts
@@ -1,47 +1,47 @@
 /**
  * Created by Administrator on 2015/12/4.
  */
-module MusicManage{
+module MusicManage {
 
-    export function Initialize():void{
+    export function Initialize(): void {
         instance.Initialize();
     }
 
-    export function PlayMusic(name:string,type?:number):void{
-        instance.PlayMusic(name,type);
+    export function PlayMusic(name: string, type?: number): void {
+        instance.PlayMusic(name, type);
     }
 
-    export function StopMusic(name):void{
+    export function StopMusic(name): void {
         instance.StopMusic(name);
     }
 
-    class MusicSys{
-        static EFFECT:number = 0;
-        static BACKGROUND:number = 1;
-        static MUSIC_GROUP:string = "music";
-        private soundLst:any = {};
-        constructor(){
+    export class MusicSys {
+        static EFFECT: number = 0;
+        static BACKGROUND: number = 1;
+        static MUSIC_GROUP: string = "music";
+        private soundLst: any = {};
+        constructor() {
 
         }
 
-        public Initialize():void{
-            var lst:RES.ResourceItem[] = RES.getGroupByName(MusicSys.MUSIC_GROUP);
-            var count:number = lst.length;
-            for( var i:number=0;i<count;i++ ){
-                var item:RES.ResourceItem = lst[i];
-                var soundNick:string = item.name;
-                var soundSrc:string = item.url;
-                this.addMusic(soundNick,soundSrc);
+        public Initialize(): void {
+            var lst: RES.ResourceItem[] = RES.getGroupByName(MusicSys.MUSIC_GROUP);
+            var count: number = lst.length;
+            for (var i: number = 0; i < count; i++) {
+                var item: RES.ResourceItem = lst[i];
+                var soundNick: string = item.name;
+                var soundSrc: string = item.url;
+                this.addMusic(soundNick, soundSrc);
             }
         }
 
-        private addMusic(name:string,src:string):any{
-            var sound:any = this.soundLst[name];
-            if( sound == void 0 ){
-                var dom:any = document.getElementsByTagName("body")[0];
-                var nDiv:any = document.createElement("div");
+        private addMusic(name: string, src: string): any {
+            var sound: any = this.soundLst[name];
+            if (sound == void 0) {
+                var dom: any = document.getElementsByTagName("body")[0];
+                var nDiv: any = document.createElement("div");
                 nDiv.style.width = "0px";
-                nDiv.style.height="0px";
+                nDiv.style.height = "0px";
                 nDiv.style.position = "absolute";
                 sound = document.createElement("audio");
                 sound.id = name;
@@ -58,45 +58,45 @@ module MusicManage{
             return sound;
         }
 
-        public PlayMusic(name:string,type?:number):void{
-            if( type == void 0 ){type=MusicSys.EFFECT}
+        public PlayMusic(name: string, type?: number): void {
+            if (type == void 0) { type = MusicSys.EFFECT }
 
-            var sound:any = this.soundLst[name];
-            if( sound != void 0 ){
-                if( type == MusicSys.EFFECT ){
+            var sound: any = this.soundLst[name];
+            if (sound != void 0) {
+                if (type == MusicSys.EFFECT) {
                     delete sound["autoplay"];
                     delete sound["loop"];
-                    sound.removeEventListener("ended",this.playFunc,false);
-                    var tag:number = 0;
-                    while( sound.paused == false ){
-                        tag ++;
-                        sound = this.addMusic(name+tag.toString(),sound.src);
+                    sound.removeEventListener("ended", this.playFunc, false);
+                    var tag: number = 0;
+                    while (sound.paused == false) {
+                        tag++;
+                        sound = this.addMusic(name + tag.toString(), sound.src);
                     }
                     sound.play();
-                }else if( type == MusicSys.BACKGROUND ){
+                } else if (type == MusicSys.BACKGROUND) {
                     sound["autoplay"] = "autoplay";
                     sound.play();
-                    sound.addEventListener("ended",this.playFunc,false);
+                    sound.addEventListener("ended", this.playFunc, false);
                 }
             }
         }
 
-        private playFunc(e:any):void{
+        private playFunc(e: any): void {
             var sound = e.target;
-            setTimeout(function(){sound.play()},100)
+            setTimeout(function() { sound.play() }, 100)
         }
 
-        public StopMusic(name:string):void{
-            var sound:any = this.soundLst[name];
-            if( sound != void 0 ){
-                if( sound.hasOwnProperty("autoplay") ){
-                    delete  sound["autoplay"];
-                    sound.removeEventListener("ended",this.playFunc,false);
+        public StopMusic(name: string): void {
+            var sound: any = this.soundLst[name];
+            if (sound != void 0) {
+                if (sound.hasOwnProperty("autoplay")) {
+                    delete sound["autoplay"];
+                    sound.removeEventListener("ended", this.playFunc, false);
                 }
                 sound.pause();
             }
         }
     }
 
-    var instance:MusicSys = new MusicSys();
+    var instance: MusicSys = new MusicSys();
 }
